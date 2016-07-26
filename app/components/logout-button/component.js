7cd726ee7bffb1fc069c3cff035b7ec28d18549b
Adam Gleichsner <amg188@case.edu>
2016-07-25 23:28:21 -0400

    Added header-nav, login and logout buttons

---
+++ app/components/logout-button/component.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  session: Ember.inject.service('session'),
+  router: Ember.inject.service('-routing'),
+
+  actions: {
+    logout() {
+      this.get('session').invalidate();
+      this.get('router').transitionTo('index');
+    }
+  }
+});
